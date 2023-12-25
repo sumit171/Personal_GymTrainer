1a47e0bc09a4343fdd7f90165b2a025a912650e9
sumit171 <sumitskkumar56@gmail.com>
2023-12-25 19:40:40 +0530

    changes for body part component

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -21,7 +21,7 @@ const BodyPart = ({ item, setBodyPart, bodyPart }) => (
           }
         : {
             background: "#fff",
-            borderBottomLeftRadius: "22px", //20px
+            borderBottomLeftRadius: "22px",
             width: "270px",
             height: "282px",
             cursor: "pointer",
